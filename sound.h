31ea1a5eb4b0e3187c4868e36fd2b2e32073bce1
Caiosenas2101 <css4@cesar.school>
2025-11-14 21:23:58 -0300

    Botao liga e desliga música

--- sound.h
+++ sound.h
@@ -1,8 +1,11 @@
 #ifndef SOUND_H
 #define SOUND_H
+#include <stdbool.h>
 
 void sound_init(void);
 void sound_update(void);
 void sound_close(void);
+void sound_toggle(void);  // Alterna entre ligar/desligar música
+bool sound_is_enabled(void);  // Retorna true se a música está ligada
 
 #endif
